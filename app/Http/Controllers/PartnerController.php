6eab51aeb70c00e71b1ecfd806c0d5f95a14732e
thangngo.dev <thangngo130802@gmail.com>
2025-05-09 13:45:00 +0700

    a

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -4,20 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Models\Contact;
 use App\Models\Relationship;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
 class PartnerController extends Controller
 {
     //
-    public function partner(){
+    public function partner()
+    {
         $relationships = Relationship::all();
         return view('contact.contact', compact('relationships'));
     }
 
     public function store(Request $request)
     {
-       
+
+        $now = Carbon::now();
+        $fifteenMinutesAgo = $now->subMinutes(15);
+
+        $existing = Contact::where(function ($query) use ($request) {
+            $query->where('email', $request->email)
+                ->orWhere('phone', $request->phone);
+        })
+            ->where('created_at', '<=', $fifteenMinutesAgo)
+            ->first();
+
+        if ($existing) {
+            return back()->with(['success' => 'Số điện thoại hoặc email đã được liên hệ trước đó. Vui lòng quay lại sau 15 phút.']);
+        }
+
         Contact::create([
             'fullname' => $request->fullname,
             'email' => $request->email,
@@ -25,7 +41,6 @@ class PartnerController extends Controller
             'message' => $request->message,
         ]);
 
-        sessionFlash('success', 'Đăng nhập thành công.');
         return back()->with('success', 'Cảm ơn bạn đã liên hệ!');
     }
 
@@ -36,12 +51,11 @@ class PartnerController extends Controller
             // Truy vấn dữ liệu từ model Relationship
             $query = Contact::query(); // Sử dụng model mặc định
             return DataTables::of($query)
-            ->addColumn('checkbox', function ($row) {
-                return '<input type="checkbox" class="row-checkbox form-check-input" value="' . $row->id . '">';
-            })
-            ->rawColumns(['checkbox'])
-            ->make(true);
-
+                ->addColumn('checkbox', function ($row) {
+                    return '<input type="checkbox" class="row-checkbox form-check-input" value="' . $row->id . '">';
+                })
+                ->rawColumns(['checkbox'])
+                ->make(true);
         }
 
         return view('backend.contact.index');
